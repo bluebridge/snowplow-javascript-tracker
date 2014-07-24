5673bc3027622a4a295516d058b8f809cff60479
Miguel Vitorino <miguel.vitorino@bluebridgeltd.com>
2014-07-24 12:23:56 +0100

    removing that stuff added before to init

--- src/js/init.js
+++ src/js/init.js
@@ -35,21 +35,5 @@
 // Snowplow Asynchronous Queue
 window._snaq = window._snaq || [];
 
-// when www.aspace.co.uk upgrade the tracker we can change the next 2 splice to push (must be before trackPageView though
-window._snaq.splice(0, 0, ['enableActivityTracking', 30, 10]);
-// our "hard-coded" collector
-window._snaq.splice(0, 0, ['setCollectorUrl', 'tag.persomi.com']);
-// if there is a trackPageView already in the queue, do not add
-(function() {
-    var i, e;
-    for (i = 0; i < window._snaq.length; i++) {
-        e = window._snaq[i];
-        if (e.length >= 1 && e[0] == 'trackPageView') {
-            return
-        }
-    }
-    window._snaq.push(['trackPageView']);
-}());
-
 var snowplow = require('./snowplow');
 window.Snowplow = window.Snowplow || new snowplow.Snowplow();
